1079c9a7c24fe8ec0c05e1c2bf8213a5a2ac51ad
LAPTOP-EV03BR1U\lannhi <nhipham9420@gmail.com>
2023-03-05 14:59:37 +0700

    init

---
+++ Test.Hangfire/Models/Driver.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.Hangfire.Models
+{
+    public class Driver
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int DriverNumber { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ Test.Hangfire/Services/ServiceManagement.cs
@@ -0,0 +1,43 @@
+﻿using Hangfire;
+using Hangfire.Storage;
+
+namespace Test.Hangfire.Services
+{
+    public class ServiceManagement : IServiceManagement
+    {
+        public void GenerateMerchandise()
+        {
+            Console.WriteLine($"Generate Merchadise: Long running task {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine($"Send mail: Long running task {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+        }
+
+        public void SyncData()
+        {
+            Console.WriteLine($"Sync data: Long running task {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+        }
+
+        public void UpdateDatabase()
+        {
+            var jobId = BackgroundJob.Schedule(
+                                        () => GenerateMerchandise(), TimeSpan.FromSeconds(30));
+            
+            var jobId2 = BackgroundJob.ContinueJobWith(
+                                        jobId,
+                                        () => Console.WriteLine($"Update database: Long running task {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}"));
+            
+            var jobId3 = BackgroundJob.ContinueJobWith(
+                                        jobId2,
+                                        () => SendMail());
+
+            // Get state of a job
+            IStorageConnection connection = JobStorage.Current.GetConnection();
+            JobData jobData = connection.GetJobData(jobId3);
+            string stateName = jobData.State;
+            Console.WriteLine(stateName);
+        }
+    }
+}
